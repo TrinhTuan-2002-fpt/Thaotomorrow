e2682c714574fcd7b041e8064fddd2118a4fbbdf
tuan <tuantu2k2badaoyl.com>
2022-06-08 11:39:58 +0700

    PluntAPI

---
+++ PerfumeShop/Models/CartDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace PerfumeShop.Models
+{
+    public class CartDetails
+    {
+        public int Amount { get; set; }
+        public double Payment { get; set; }
+        public int Status { get; set; }
+        public int CartId { get; set; }
+        public Carts Carts { get; set; }
+        public int ProductId { get; set; }
+        public Products Products { get; set; }
+    }
+}
