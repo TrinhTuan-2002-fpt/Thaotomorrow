71fc9554bc94c9d1f0f0f9fc0cfe35e09a6e0e68
Nguyen thanh trung <trungfbi1@gmail.com>
2022-06-15 21:13:45 +0700

    cusaddress

---
+++ PerfumeShop/Areas/Admin/Controllers/ViewController/AddressesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PerfumeShop.Models;
+
+namespace PerfumeShop.Areas.Admin.Controllers.ViewController
+{
+    [Area("Admin")]
+    public class AddressesController : Controller
+    {
+        private readonly DBContext _context;
+
+        public AddressesController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Addresses
+        public async Task<IActionResult> Index()
+        {
+              return _context.Address != null ? 
+                          View(await _context.Address.ToListAsync()) :
+                          Problem("Entity set 'DBContext.Address'  is null.");
+        }
+
+        // GET: Admin/Addresses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Address == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address
+                .FirstOrDefaultAsync(m => m.AddressId == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // GET: Admin/Addresses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Addresses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AddressId,City")] Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(address);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(address);
+        }
+
+        // GET: Admin/Addresses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Address == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            return View(address);
+        }
+
+        // POST: Admin/Addresses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AddressId,City")] Address address)
+        {
+            if (id != address.AddressId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(address);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AddressExists(address.AddressId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(address);
+        }
+
+        // GET: Admin/Addresses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Address == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Address
+                .FirstOrDefaultAsync(m => m.AddressId == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // POST: Admin/Addresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Address == null)
+            {
+                return Problem("Entity set 'DBContext.Address'  is null.");
+            }
+            var address = await _context.Address.FindAsync(id);
+            if (address != null)
+            {
+                _context.Address.Remove(address);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AddressExists(int id)
+        {
+          return (_context.Address?.Any(e => e.AddressId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ PerfumeShop/Areas/Admin/Controllers/ViewController/CustomersController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PerfumeShop.Models;
+
+namespace PerfumeShop.Areas.Admin.Controllers.ViewController
+{
+    [Area("Admin")]
+    public class CustomersController : Controller
+    {
+        private readonly DBContext _context;
+
+        public CustomersController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Customers
+        public async Task<IActionResult> Index()
+        {
+            var dBContext = _context.Customers.Include(c => c.Address);
+            return View(await dBContext.ToListAsync());
+        }
+
+        // GET: Admin/Customers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customers = await _context.Customers
+                .Include(c => c.Address)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customers == null)
+            {
+                return NotFound();
+            }
+
+            return View(customers);
+        }
+
+        // GET: Admin/Customers/Create
+        public IActionResult Create()
+        {
+            ViewData["AddressId"] = new SelectList(_context.Address, "AddressId", "AddressId");
+            return View();
+        }
+
+        // POST: Admin/Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerId,Name,Email,Password,Gender,PhoneNumber,Status,AddressId")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customers);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Address, "AddressId", "AddressId", customers.AddressId);
+            return View(customers);
+        }
+
+        // GET: Admin/Customers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customers = await _context.Customers.FindAsync(id);
+            if (customers == null)
+            {
+                return NotFound();
+            }
+            ViewData["AddressId"] = new SelectList(_context.Address, "AddressId", "AddressId", customers.AddressId);
+            return View(customers);
+        }
+
+        // POST: Admin/Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CustomerId,Name,Email,Password,Gender,PhoneNumber,Status,AddressId")] Customers customers)
+        {
+            if (id != customers.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customers);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomersExists(customers.CustomerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Address, "AddressId", "AddressId", customers.AddressId);
+            return View(customers);
+        }
+
+        // GET: Admin/Customers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customers = await _context.Customers
+                .Include(c => c.Address)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customers == null)
+            {
+                return NotFound();
+            }
+
+            return View(customers);
+        }
+
+        // POST: Admin/Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Customers == null)
+            {
+                return Problem("Entity set 'DBContext.Customers'  is null.");
+            }
+            var customers = await _context.Customers.FindAsync(id);
+            if (customers != null)
+            {
+                _context.Customers.Remove(customers);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomersExists(int id)
+        {
+          return (_context.Customers?.Any(e => e.CustomerId == id)).GetValueOrDefault();
+        }
+    }
+}
